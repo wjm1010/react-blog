2867a3520a7d3f9a96acea8f15b98c95fd30e9c8
wjm <wangjm@kaiyufound.com>
2019-12-23 17:45:09 +0800

    code

--- blog/components/header.js
+++ blog/components/header.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import '../static/style/components/header.css'
-
 import { Row, Col, Menu, Icon } from 'antd'
+import Router from 'next/router'
+import Link from 'next/link'
+import axios from 'axios'
+import Api from '../config/api'
 
-const header = () => (
-  <div className="header">
-    <Row type="flex" justify="center">
-      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
-        <span className="header-logo">hello</span>
-        <span className="header-text">Ant Design</span>
-      </Col>
-      <Col className="menu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
-        <Menu mode="horizontal">
-          <Menu.Item key="home">
-            <Icon type="home"/>
+const header = () => {
+  const [navArray, setNavArray] = useState([])
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const result= await axios(Api.getTypeInfo).then(res=>{
+        setNavArray(res.data.data)
+        return res.data.data
+      })
+      setNavArray(result)
+    }
+    fetchData()
+  },[])
+  const handleClick = (e)=>{
+    if(e.key=='0'){
+      Router.push('/index')
+    }else{
+      Router.push('/list?id='+e.key)
+    }
+  }
+  return (
+    <div className="header">
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+          <span className="header-logo">hello</span>
+          <span className="header-text">Ant Design</span>
+        </Col>
+        <Col className="menu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
+          <Menu mode="horizontal" onClick={handleClick}>
+          <Menu.Item key='0'>
+            <Icon type="home" />
             首页
           </Menu.Item>
-          <Menu.Item key="video">
-            <Icon type="youtube"/>
-            视频
-          </Menu.Item>
-          <Menu.Item key="life">
-            <Icon type="smile"/>
-            生活
-          </Menu.Item>
-        </Menu>
-      </Col>
-    </Row>
-  </div>
-)
+            {
+              navArray.map((item)=>{
+                return(
+                  <Menu.Item key={item.Id}>
+                    <Icon type={item.icon} />
+                    {item.typeName}
+                  </Menu.Item>
+                )
+              }) 
+            }
+          </Menu>
+        </Col>
+      </Row>
+    </div>
+  )
+}
 export default header
--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -42,6 +42,33 @@ class HomeController extends Controller {
 		const result = await this.app.mysql.query(sql)
 		this.ctx.body = { data: result }
 	}
+
+	//得到类别名称和编号
+  async getTypeInfo(){
+		const result = await this.app.mysql.select('type')
+		this.ctx.body = { data: result }
+	}
+
+	async getListById() {
+		
+		let id = this.ctx.params.id
+		let sql = 'SELECT article.id as id,' +
+			'article.title as title,' +
+			'article.article_content as article_content,' +
+			"FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime," +
+			'article.view_count as view_count,' +
+			'article.content as content,' +
+			'type.typeName as typeName,' +
+			'type.id as typeId ' +
+			'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+			'WHERE type_id=' + id
+
+		const results = await this.app.mysql.query(sql)
+
+		this.ctx.body = {
+			data: results
+		}
+	}
 }
 
 module.exports = HomeController
